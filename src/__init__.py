4bad9e74c21495452d684c0006133693462746db
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2022-08-28 13:43:35 -0300

    feat: first file

---
+++ src/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# ------------------------------------------------------------------------------
+#  Name: __init__.py
+#  Version: 0.0.1
+#
+#  Summary: Python Project Settings
+#           The basic structure for a Python project.
+#
+#  Author: Alexsander Lopes Camargos
+#  Author-email: alcamargos@vivaldi.net
+#
+#  License: MIT
+# ------------------------------------------------------------------------------
